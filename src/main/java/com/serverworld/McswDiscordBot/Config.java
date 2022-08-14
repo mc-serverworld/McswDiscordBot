9f3e68d1c321973228bf943d0ad844f645f240d7
admin <paul002706@gmail.com>
2022-08-14 21:15:59 +0800

    brrrrr

--- src/main/java/com/serverworld/McswDiscordBot/Config.java
+++ src/main/java/com/serverworld/McswDiscordBot/Config.java
@@ -1,7 +1,24 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ *     Copyright (C) 2022-2022 chiliasmstudio
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.serverworld.McswDiscordBot;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
